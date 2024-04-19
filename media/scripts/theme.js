7f8301a7577a3cbf9a31dc0cc37b2f04e9c4868a
ryanleeman <mail@ryanmann.org>
2024-04-20 08:47:13 +1000

    formatting.

--- media/scripts/theme.js
+++ media/scripts/theme.js
@@ -12,15 +12,10 @@
     if (storedTheme) {
       return storedTheme;
     }
-    return window.matchMedia("(prefers-color-scheme: dark)").matches
-      ? "dark"
-      : "light";
+    return window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light";
   };
   const setTheme = (theme) => {
-    if (
-      theme === "auto" &&
-      window.matchMedia("(prefers-color-scheme: dark)").matches
-    ) {
+    if (theme === "auto" && window.matchMedia("(prefers-color-scheme: dark)").matches) {
       document.documentElement.setAttribute("data-bs-theme", "dark");
     } else {
       document.documentElement.setAttribute("data-bs-theme", theme);
@@ -32,20 +27,16 @@
       element.classList.remove("active");
       element.setAttribute("aria-pressed", "false");
     });
-    const btnToActive = document.querySelector(
-      `[data-bs-theme-value="${theme}"]`,
-    );
+    const btnToActive = document.querySelector(`[data-bs-theme-value="${theme}"]`);
     btnToActive.classList.add("active");
     btnToActive.setAttribute("aria-pressed", "true");
   };
-  window
-    .matchMedia("(prefers-color-scheme: dark)")
-    .addEventListener("change", () => {
-      const storedTheme = getStoredTheme();
-      if (storedTheme !== "light" && storedTheme !== "dark") {
-        setTheme(getPreferredTheme());
-      }
-    });
+  window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", () => {
+    const storedTheme = getStoredTheme();
+    if (storedTheme !== "light" && storedTheme !== "dark") {
+      setTheme(getPreferredTheme());
+    }
+  });
   window.addEventListener("DOMContentLoaded", () => {
     showActiveTheme(getPreferredTheme());
     document.querySelectorAll("[data-bs-theme-value]").forEach((toggle) => {
